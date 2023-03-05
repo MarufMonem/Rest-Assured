28750c17124ce28a1b354933e05a57ee19dba3b7
Maruf_Monem <marufmonem@gmail.com>
2023-03-05 08:51:42 +0600

    Send and validate a PUT request: Google Maps Add Location API

---
+++ src/main/java/BasicApiTest_PUT.java
@@ -0,0 +1,39 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class BasicApiTest_PUT {
+    public static void main(String[] args) {
+
+//    1. **Given**: it takes all the input details needed for an API
+//    2. **When**: Submit the API
+//    3. **There**: Validate the response
+
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+//        to use given we need to add the static package
+        given()
+                .log().all()
+                .queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "    \"location\": {\n" +
+                        "        \"lat\": -38.383494,\n" +
+                        "        \"lng\": 33.427362\n" +
+                        "    },\n" +
+                        "    \"accuracy\": 50,\n" +
+                        "    \"name\": \"Frontline house\",\n" +
+                        "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "    \"address\": \"29, side layout, cohen 09\",\n" +
+                        "    \"types\": [\n" +
+                        "        \"shoe park\",\n" +
+                        "        \"shop\"\n" +
+                        "    ],\n" +
+                        "    \"website\": \"http://google.com\",\n" +
+                        "    \"language\": \"French-IN\"\n" +
+                        "}")
+                .when()
+                .post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200);
+
+
+    }
+}
