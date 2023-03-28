97fbf1a99edcc93f9a53993cf8a4ff06641652cc
Maruf_Monem <marufmonem@gmail.com>
2023-03-28 08:59:06 +0600

    Added and verified newly created product

--- src/main/java/Ecommerce_test/ECommerceAPITest.java
+++ src/main/java/Ecommerce_test/ECommerceAPITest.java
@@ -3,12 +3,16 @@ package Ecommerce_test;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 
 public class ECommerceAPITest {
     public static void main(String[] args) {
 
-        RequestSpecification req = new RequestSpecBuilder()
+        RequestSpecification baseLoginReq = new RequestSpecBuilder()
                 .setBaseUri("https://rahulshettyacademy.com")
                 .setContentType(ContentType.JSON).build();
 
@@ -18,10 +22,32 @@ public class ECommerceAPITest {
         lr.setUserPassword("Test123456");
 
 //        Making the request
-        RequestSpecification reqlogin = given().log().all().spec(req).body(lr);
-        loginResponse response = reqlogin.when().log().all().post("/api/ecom/auth/login").then().extract().response().as(loginResponse.class);
+        RequestSpecification reqlogin = given().log().all().spec(baseLoginReq).body(lr);
+        loginResponse loginResponse = reqlogin.when().log().all().post("/api/ecom/auth/login").then().extract().response().as(loginResponse.class);
 
-        System.out.println("The token is: " + response.getToken());
+        System.out.println("The token is: " + loginResponse.getToken());
+
+//        Create product
+//        ------------- FORM PARAMETERS ----------------
+        RequestSpecification addProductBaseReq = new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com")
+                .addHeader("Authorization", loginResponse.getToken()).build();
+
+        RequestSpecification addProductReq = given().spec(addProductBaseReq)
+                .param("productName", "Laptop")
+                .param("productAddedBy", loginResponse.getUserId())
+                .param("productCategory", "Tech")
+                .param("productSubCategory", "Portable")
+                .param("productPrice", "6900")
+                .param("productDescription", "HP")
+                .param("productFor", "Students")
+                .multiPart("productImage", new File("C:\\Users\\Anik\\Desktop\\camera.jpg"));
+
+        Product newlyCreatedProduct = addProductReq.when().post("api/ecom/product/add-product").then().log().all().extract().as(Product.class);
+
+        Assert.assertEquals("Product Added Successfully", newlyCreatedProduct.getMessage());
+
+        System.out.println("The product ID is: " + newlyCreatedProduct.getProductId());
 
 
     }
---
+++ src/main/java/Ecommerce_test/Product.java
@@ -0,0 +1,22 @@
+package Ecommerce_test;
+
+public class Product {
+    private String message;
+    private String productId;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
